5542be67fcb8bbb6d219251ccce383cef51e1bb9
Haranycheva Mariia <mimimashkabumazka@gmail.com>
2024-03-16 18:13:52 +0200

    Update FormToDo.jsx

--- src/FormToDo/FormToDo.jsx
+++ src/FormToDo/FormToDo.jsx
@@ -1,74 +1,76 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import * as Yup from "yup";
-import { Title, FormBtn, FormToDO, InputText, Select} from "./FormToDo.styled";
+import { Title, FormBtn, FormToDO, InputText, Select } from "./FormToDo.styled";
 import { Formik, ErrorMessage } from "formik";
+
 const schemaValidation = Yup.object({
   title: Yup.string()
-    .min(2, "must be min 2 simbols")
-    .required("Title must be required"),
-  description: Yup.string().required("Title must be required"),
-  level: Yup.string().oneOf(["easy", "medium", "hard"]).required("Title must be required"),
+    .min(2, "Must be at least 2 characters")
+    .required("Title is required"),
+  description: Yup.string().required("Description is required"),
+  level: Yup.string()
+    .oneOf(["easy", "medium", "hard"], "Invalid level")
+    .required("Level is required"),
 });
+
 export class FormToDo extends Component {
-  // handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   this.props.onAdd(this.state);
-  //   this.setState(initialValue);
-  // };
+  handleInputChange = (e, handleChange) => {
+    handleChange(e);
+    localStorage.setItem(e.target.name, JSON.stringify(e.target.value));
+  };
 
   render() {
     return (
       <Formik
-        initialValues={{ title: "", description: "", level: 1 }}
+        initialValues={{
+          title: JSON.parse(localStorage.getItem("title")) || "",
+          description: JSON.parse(localStorage.getItem("description")) || "",
+          level: JSON.parse(localStorage.getItem("level")) || "easy",
+        }}
         validationSchema={schemaValidation}
-        onSubmit={(values, {resetForm} ) => {
-          console.log(values)
-          resetForm({ title: "", description: "", level: 1 })
-          this.props.onAdd(values)
+        onSubmit={(values, { resetForm }) => {
+          resetForm({ title: "", description: "", level: "easy" });
+          localStorage.setItem("title", JSON.stringify(""));
+          localStorage.setItem("description", JSON.stringify(""));
+          localStorage.setItem("level", JSON.stringify("easy"));
+          this.props.onAdd(values);
+          
         }}
       >
-        <FormToDO>
-          <Title>enter your data</Title>
-          <InputText name="title" placeholder="enter title" />
-          <ErrorMessage name="title"></ErrorMessage>
-          <InputText
-            name="description"
-            type="text"
-            placeholder="enter description"
-          />
-          <ErrorMessage name="description" />
-          <Select name="level" as="select">
-            <option value="1">level 1</option>
-            <option value="2">level 2</option>
-            <option value="3">level 3</option>
-          </Select>
-          <FormBtn type="submit">Add</FormBtn>
-        </FormToDO>
+        {({ values, handleChange }) => (
+          <>
+            <FormToDO>
+              <Title>Enter your data</Title>
+              <InputText
+                name="title"
+                placeholder="Enter title"
+                onChange={(e) => this.handleInputChange(e, handleChange)}
+                value={values.title}
+              />
+              <ErrorMessage name="title" />
+              <InputText
+                name="description"
+                type="text"
+                placeholder="Enter description"
+                onChange={(e) => this.handleInputChange(e, handleChange)}
+                value={values.description}
+              />
+              <ErrorMessage name="description" />
+              <Select
+                name="level"
+                as="select"
+                onChange={(e) => this.handleInputChange(e, handleChange)}
+                value={values.level}
+              >
+                <option value="easy">Level 1</option>
+                <option value="medium">Level 2</option>
+                <option value="hard">Level 3</option>
+              </Select>
+              <FormBtn type="submit">Add</FormBtn>
+            </FormToDO>
+          </>
+        )}
       </Formik>
     );
-    // <Form onSubmit={this.handleSubmit}>
-    //   <Title>enter your data</Title>
-    //   <InputToDo
-    //     name="title"
-    //     placeholder="enter title"
-    //     value={this.state.title}
-    //     onChange={this.handleChange}
-    //   />
-    //   <InputToDo
-    //     name="description"
-    //     type="text"
-    //     placeholder="enter description"
-    //     value={this.state.description}
-    //     onChange={this.handleChange}
-    //   />
-    //   <input type="checkbox" checked={this.state.agree} onChange={this.handleChangeCheckBox}/>
-    //   <Select value={this.state.level} onChange={this.handleChangeSelect}>
-    //     <option value="1">level 1</option>
-    //     <option value="2">level 2</option>
-    //     <option value="3">level 3</option>
-    //   </Select>
-    //   <FormBtn type="submit">Add</FormBtn>
-    // </Form>
-    // );
   }
 }
